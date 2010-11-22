d37d6350be2bdba6a90877f1fea846f2607b4d86
admin <admin@7ac592ef-df4f-1d4a-b35f-111f04560860>
2010-11-22 14:34:25 +0000

    Changed poll from setInterval to setTimeout to prevent excessive requests.
    git-svn-id: svn://localhost:8080@25 7ac592ef-df4f-1d4a-b35f-111f04560860

--- pyChat/html/http.js
+++ pyChat/html/http.js
@@ -103,48 +103,52 @@
         xmlhttp = createXMLHttpRequest();
         xmlhttp.onreadystatechange = function()
         {
-            if (xmlhttp.readyState==4 && xmlhttp.status==200)
-            {
-                var scr_to_bottom = (($("#MessageList").attr("scrollTop")+
-                    $("#MessageList").outerHeight()) - $("#MessageList").attr("scrollHeight"));
+            if (xmlhttp.readyState==4) {
+                if (xmlhttp.status==200)
+                {
+                    var scr_to_bottom = (($("#MessageList").attr("scrollTop")+
+                        $("#MessageList").outerHeight()) - $("#MessageList").attr("scrollHeight"));
 
-                var obj = jQuery.parseJSON(xmlhttp.responseText);
-                var tmpstring = "<table cellspacing='0'>";
-                var i, N = obj.length;
-                for (i=0; i<N; i++) {
-                    if (obj[i][0] == 0) {
-                        var msg = new String();
-                        msg += "<tr><td class='header'>";
-                        msg += "<b>"+obj[i][1]+"</b></td>";
-                        msg += "<td class='time'>"+obj[i][2]+"</td>";
-                        //msg += "<td class='header3'></td>";
-                        msg += "</tr>";
-                        msg += "<tr><td class='body' colspan='2'><blockquote>"+obj[i][3]+"</blockquote></td></tr>";
-                        msg += "";
-                        tmpstring += msg;
-                        if (obj[i][4] > timestamp) timestamp = obj[i][4];
+                    var obj = jQuery.parseJSON(xmlhttp.responseText);
+                    var tmpstring = "<table cellspacing='0'>";
+                    var i, N = obj.length;
+                    for (i=0; i<N; i++) {
+                        if (obj[i][0] == 0) {
+                            var msg = new String();
+                            msg += "<tr><td class='header'>";
+                            msg += "<b>"+obj[i][1]+"</b></td>";
+                            msg += "<td class='time'>"+obj[i][2]+"</td>";
+                            //msg += "<td class='header3'></td>";
+                            msg += "</tr>";
+                            msg += "<tr><td class='body' colspan='2'><blockquote>"+obj[i][3]+"</blockquote></td></tr>";
+                            msg += "";
+                            tmpstring += msg;
+                            if (obj[i][4] > timestamp) timestamp = obj[i][4];
+                        }
+                    }
+                    tmpstring += "</table>";
+                    $("#MessageList").append(tmpstring);
+
+                    // is scroll is at bottom, scroll to bottom
+                    if (scr_to_bottom >= 0) {
+                        $("#MessageList").attr({scrollTop: $("#MessageList").attr("scrollHeight")});
+                        //document.getElementById("dbg").innerHTML = scr_to_bottom;
+                    } else {
+                        //document.getElementById("dbg").innerHTML = "don't scroll";
                     }
                 }
-                tmpstring += "</table>";
-                $("#MessageList").append(tmpstring);
-
-                // is scroll is at bottom, scroll to bottom
-                if (scr_to_bottom >= 0) {
-                    $("#MessageList").attr({scrollTop: $("#MessageList").attr("scrollHeight")});
-                    //document.getElementById("dbg").innerHTML = scr_to_bottom;
-                } else {
-                    //document.getElementById("dbg").innerHTML = "don't scroll";
+                else if (xmlhttp.status==204)
+                {
+                    $("#Login").fadeOut();
+                    resizeUI(500);
                 }
-            }
-            else if (xmlhttp.readyState==4 && xmlhttp.status==204)
-            {
-                $("#Login").fadeOut();
-                resizeUI(500);
-            }
-            else if (xmlhttp.readyState==4 && xmlhttp.status==401)
-            {
-                $("#Login").fadeIn();
-                resizeUI(0);
+                else if (xmlhttp.status==401)
+                {
+                    $("#Login").fadeIn();
+                    resizeUI(0);
+                }
+
+                timer = setTimeout(poll, 500);
             }
         }
         params = timestamp + "";
@@ -159,5 +163,4 @@
     function init_poll()
     {
         poll();
-        timer = setInterval(poll, 500);
     }
