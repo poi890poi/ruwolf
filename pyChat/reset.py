842652f719ec3e283bc52ab3a6afd6216c30f3f0
Lin Lee <poi890poi@gmail.com>
2011-01-06 21:40:59 +0800

    Added reset.py to drop all tables.

---
+++ pyChat/reset.py
@@ -0,0 +1,20 @@
+import os
+import sqlite3
+
+from constant import *
+
+# init database
+
+appdata = os.path.join(os.environ['APPDATA'], APPDATA_FOLDER)
+if not os.path.isdir(appdata):
+    os.mkdir(appdata)
+dbf = os.path.join(appdata, DB_FILENAME)
+conn = sqlite3.connect(dbf)
+dbcursor = conn.cursor()
+
+# Create table
+dbcursor.execute('''drop table message''')
+dbcursor.execute('''drop table user''')
+dbcursor.execute('''drop table room''')
+dbcursor.execute('''drop table action''')
+dbcursor.execute('''drop table ruleset''')
